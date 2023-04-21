9a46b49baf1bd1e63497267034c6242f1070f97a
Daniel Bodnar <1790726+DanielBodnar@users.noreply.github.com>
2023-04-21 18:46:28 -0500

    configuring for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -19,10 +21,13 @@ const config = {
             // these options are set automatically — see below
             pages: 'build',
             assets: 'build',
-            fallback: 'index.html',
+            fallback: null,
             precompress: false,
             strict: true
         }),
+		paths: {
+            base: dev ? '' : process.env.BASE_PATH || '',
+        },
 		prerender:{
 		  crawl:true,
 		  entries: ["*", "/", "/reentry", "/open", "/release","/refocus","/about","/terms-of-use","/nudge-bug-terms","/full_tos","/tos"],
