bd232eb16f190aeb33b179481a35c907ae75d6c6
Daniel Bodnar <1790726+DanielBodnar@users.noreply.github.com>
2023-04-25 00:02:37 -0500

    trying to fix static rendering

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 //import adapter from '@sveltejs/adapter-auto';
 
 //import adapter from 'svelte-adapter-deno';
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
@@ -25,7 +24,6 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	compilerOptions: {},
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
